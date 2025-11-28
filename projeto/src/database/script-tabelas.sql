e26a5c0ead60dbe509e1ad9cc2bbaabc3ac08e80
laviniafaria <lavinia.faria@sptech.school>
2025-11-28 16:35:34 -0300

    Update SQL script to enforce NOT NULL constraints

--- projeto/src/database/script-tabelas.sql
+++ projeto/src/database/script-tabelas.sql
@@ -1,45 +1,45 @@
 
 CREATE DATABASE baralho_cigano;
-
 USE baralho_cigano;
-
 
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50) UNIQUE,
-	senha VARCHAR(50)
-);
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+	nome VARCHAR(50) NOT NULL,
+	email VARCHAR(50) UNIQUE NOT NULL,
+	senha VARCHAR(50) NOT NULL
 );
 
+select * from usuario;
+
 CREATE TABLE quiz (
-id INT PRIMARY KEY AUTO_INCREMENT,
-titulo VARCHAR(100),
-descricao TEXT
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	titulo VARCHAR(100) NOT NULL,
+	descricao TEXT
 );
 
 CREATE TABLE respostas (
-id INT PRIMARY KEY AUTO_INCREMENT,
-usuario_id INT,
-quiz_id INT,
-resposta TEXT,
-FOREIGN KEY (usuario_id) REFERENCES usuario(id),
-FOREIGN KEY (quiz_id) REFERENCES quiz(id)
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	usuario_id INT NOT NULL,
+	quiz_id INT NOT NULL,
+	resposta_certas INT NOT NULL,
+	resposta_erradas INT NOT NULL,
+	FOREIGN KEY (usuario_id) REFERENCES usuario(id),
+	FOREIGN KEY (quiz_id) REFERENCES quiz(id)
 );
+
+select * from respostas;
 
 CREATE TABLE ranqueamento (
-usuario_id INT,
-quiz_id INT,
-pontuacao INT,
-PRIMARY KEY (usuario_id, quiz_id),
-FOREIGN KEY (usuario_id) REFERENCES usuario(id),
-FOREIGN KEY (quiz_id) REFERENCES quiz(id)
+	usuario_id INT NOT NULL,
+	quiz_id INT NOT NULL,
+	pontuacao INT NOT NULL,
+	PRIMARY KEY (usuario_id, quiz_id),
+	FOREIGN KEY (usuario_id) REFERENCES usuario(id),
+	FOREIGN KEY (quiz_id) REFERENCES quiz(id)
 );
-select * from usuario;
 
+SELECT u.nome, r.resposta_erradas, r.resposta_certas
+	FROM usuario AS u JOIN respostas AS r
+		ON u.id = r.usuario_id
+			GROUP BY u.nome, r.resposta_certas, r.resposta_erradas
+				ORDER BY resposta_certas DESC LIMIT 5;
